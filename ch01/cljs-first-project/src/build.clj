9a66a5fcfc112c2ac3ad754f96e3d26add4d4de8
Larry Jones <larryj@blinds.com>
2017-05-09 19:18:11 -0500

    Specify entry point in build configuraton

--- ch01/cljs-first-project/src/build.clj
+++ ch01/cljs-first-project/src/build.clj
@@ -1,6 +1,11 @@
 (require 'cljs.build.api)
 
 (cljs.build.api/build "src"
-                      {:output-to "out/main.js"}) ;; The path of the JavaScript file to output.
-                       
+                      {
+                       ;; The program entry point. `goog` loads and executes this module / namespace.
+                       ;; By specifying the entry point, I *need not* specify it as the text of the last
+                       ;; `script` tag.
+                       :main 'cljs-first-project.core 
+                       ;; The path of the JavaScript file to output.
+                       :output-to "out/main.js"}) 
 
