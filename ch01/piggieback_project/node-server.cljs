4ee73296cbf94b5ab1a0eba590757cef20ac7ede
Larry Jones <larryj@blinds.com>
2017-05-11 08:25:40 -0500

    REPL connected to Node.js
    This particular commit uses the thread first macro (`->`) to more
    effectively communicate code to "procedural eyes."

--- ch01/piggieback_project/node-server.cljs
+++ ch01/piggieback_project/node-server.cljs
@@ -2,17 +2,18 @@
 
 (def http (js/require "http"))
 
-(.listen (.createServer http
-                        (fn [req res]
-                          (do
-                            (.writeHead res
-                                        200 ;; Report success
-                                        (js-obj "Content-Type" 
-                                                "text/plain" ;; Return plain text
-                                                ))
-                            (.end res
-                                  "Hello World from Node.js!" ;; response content
-                                  ))))
-         1337 ;; listen on port 
-         "127.0.0.1" ;; on IP address (localhost)
-         )
+;; Use threading macro to better communicate procedural code
+(-> http
+    (.createServer (fn [req res]
+                     (do
+                       (.writeHead res
+                                   200 ;; Report success
+                                   (js-obj "Content-Type" 
+                                           "text/plain" ;; Return plain text
+                                           ))
+                       (.end res
+                             "Hello World from Node.js!" ;; response content
+                             ))))
+    (.listen 1337 ;; listen on port
+             "127.0.0.1" ;; on IP address (localhost)
+             )) 
